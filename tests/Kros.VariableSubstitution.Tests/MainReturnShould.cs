aeabd0220b1d7846da104ecc8ed1499878cadf6d
Daniel Caban <caban@kros.sk>
2022-03-16 11:12:18 +0100

    renamed method

--- tests/Kros.VariableSubstitution.Tests/MainReturnShould.cs
+++ tests/Kros.VariableSubstitution.Tests/MainReturnShould.cs
@@ -7,14 +7,14 @@ namespace Kros.VariableSubstitution.Tests
     public class MainReturnShould
     {
         [Theory]
-        [MemberData(nameof(GetArgs))]
+        [MemberData(nameof(TestDifferentArgs_Data))]
         public void TestDifferentArgs(string[] args, int expectedReturn)
         {
             var actual = Program.Main(args);
             actual.Should().Be(expectedReturn);
         }
 
-        public static IEnumerable<object[]> GetArgs()
+        public static IEnumerable<object[]> TestDifferentArgs_Data()
         {
             yield return new object[] { new string[] { }, ExitCodes.MissingWorkingDirectory };
             yield return new object[] { new string[] { "-w", "placeholder", "-v", "incorrectVariable" }, ExitCodes.WrongVariablesFormat };
